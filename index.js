33f340080216c9d1940e16e84527dc8627f21218
TechnoDogs-3707 <qwrty.streams@gmail.com>
2025-12-20 19:59:48 -0500

    so much.....

--- index.js
+++ index.js
@@ -1,30 +1,63 @@
+// const cName = document.getElementById("name");
+
+// let intervalId;
+// let lastDegree;
+// let degree;
+
+// let textRotate = 10;
+
+// cName.addEventListener("mouseenter", () => {
+//     const intervalDelay = 150;
+//     intervalId = setInterval(rotateName, intervalDelay);
+// });
+
+// cName.addEventListener("mouseleave", () => {
+//     clearInterval(intervalId);
+//     cName.style.transform = "rotate(0deg)";
+// });
+
+// function rotateName() {
+//     lastDegree = degree;
+//     degree = randomDegree(-textRotate, textRotate);
+//     let val = Math.abs(degree - lastDegree);
+//     if (val < textRotate) {
+//         rotateName();
+//     } else {
+//         cName.style.transform = `rotate(${degree}deg)`;
+//     }
+// }
+
+// function randomDegree(min, max) {
+//     return Math.floor(Math.random() * ((max + 1) - min) + min);
+// }
+
 const cName = document.getElementById("name");
+const fursonaModal = document.getElementById("fursona-modal");
 
-let intervalId;
-let lastDegree;
-let degree;
+let open = false;
+let isAnimating = false;
 
-cName.addEventListener("mouseenter", () => {
-    const intervalDelay = 150;
-    intervalId = setInterval(rotateName, intervalDelay);
+cName.addEventListener("click", (event) => {
+    event.stopPropagation();
+    if (open || isAnimating) return;
+    fursonaModal.classList.add("is-open");
+    open = true;
 });
 
-cName.addEventListener("mouseleave", () => {
-    clearInterval(intervalId);
-    cName.style.transform = "rotate(0deg)";
+fursonaModal.addEventListener("click", (event) => {
+    event.stopPropagation();
 });
 
-function rotateName() {
-    lastDegree = degree;
-    degree = randomDegree(-7, 7);
-    let val = Math.abs(degree - lastDegree);
-    if (val < 7) {
-        rotateName();
-    } else {
-        cName.style.transform = `rotate(${degree}deg)`;
-    }
-}
+window.addEventListener("click", () => {
+    if (!open || isAnimating) return;
+    isAnimating = true;
+    fursonaModal.classList.remove("is-open");
+    fursonaModal.classList.add("is-closing");
+});
 
-function randomDegree(min, max) {
-    return Math.floor(Math.random() * ((max + 1) - min) + min);
-}
+fursonaModal.addEventListener("animationend", (event) => {
+    if (event.animationName !== "popout") return;
+    fursonaModal.classList.remove("is-closing");
+    open = false;
+    isAnimating = false;
+});
