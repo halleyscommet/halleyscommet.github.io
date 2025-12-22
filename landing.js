896a88361d56bd3324ff9f92df7f7bce77241f2a
TechnoDogs-3707 <nharp335@gmail.com>
2025-12-21 19:30:20 -0500

    Update landing page structure and styles for improved functionality and design consistency

--- landing.js
+++ landing.js
@@ -1,16 +1,28 @@
 const cName = document.getElementById("name");
 const fursonaModal = document.getElementById("fursona-modal");
-const timeSpan = document.getElementById("time");
+const timeSpans = document.getElementsByClassName("time");
+const aboutMe = document.getElementById("about-me");
 
 let open = false;
 let isAnimating = false;
-
 
 const options = {
     hour: "2-digit",
     minute: "2-digit",
     second: "2-digit",
     hour12: false
+}
+
+const birthday = new Date(2009, 5, 24);
+const age = getAge(birthday);
+
+const info = {
+    user: "haley",
+    age: age,
+    work: "mentor@tcsPlymouth",
+    learning: "gamedev,C",
+    theme: "dark",
+    editor: "nvim"
 }
 
 cName.addEventListener("click", (event) => {
@@ -22,7 +34,20 @@ cName.addEventListener("click", (event) => {
     const now = new Date();
     const formattedTime = new Intl.DateTimeFormat('en-GB', options).format(now);
 
-    timeSpan.innerText = formattedTime;
+    for (let i = 0; i < timeSpans.length; i++) {
+        const span = timeSpans[i];
+        span.innerText = formattedTime;
+    }
+
+    aboutMe.innerHTML = "";
+    let iter = 0;
+    Object.entries(info).forEach(([key, value]) => {
+        aboutMe.innerHTML += `<p>${key.toUpperCase()}=${value}</p>`;
+        iter++;
+        if (iter % 2 == 0 && iter != 6) {
+            aboutMe.innerHTML += "<br>";
+        }
+    });
 });
 
 fursonaModal.addEventListener("click", (event) => {
@@ -42,3 +67,16 @@ fursonaModal.addEventListener("animationend", (event) => {
     open = false;
     isAnimating = false;
 });
+
+function getAge(birthDate, currentDate = new Date()) {
+    let age = currentDate.getFullYear() - birthDate.getFullYear();
+
+    const hasHadBirthdayThisYear =
+        currentDate.getMonth() > birthDate.getMonth() ||
+        (currentDate.getMonth() === birthDate.getMonth() &&
+            currentDate.getDate() >= birthDate.getDate());
+
+    if (!hasHadBirthdayThisYear) age--;
+
+    return age;
+}
